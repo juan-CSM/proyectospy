a4ec0e8399d382ca7152cf2212e16e37a030e178
Juan Carlos 1987 <k.juno.1987@gmail.com>
2019-02-07 01:57:25 -0600

    Se agrego la fecha al archivo base

--- creabase.py
+++ creabase.py
@@ -2,19 +2,24 @@
 
 import os
 import sys
+from datetime import datetime
 
 direccion = os.getcwd()
+fecha = str(datetime.now())
 for i in sys.argv[1:]:
     try:
         os.mkdir(i)
         os.chdir(i)
         with open("base.py", "w") as f:
             f.write("#!/usr/bin/env python3\n\n")
+            f.write(f"# Base para el programa {i}\n")
             f.write("# Escrito por: Juan Carlos 1987\n")
             f.write("# Correo: k.juno.1987@gmail.com\n")
+            f.write(f"# Fecha: {fecha}\n")
             f.write("# \n")
         os.chdir(direccion)
         print(f"El archivo {i} se creo correctamente")
     except:
         print("Ocurrio alun problema")
+        print(f"la carpeta {i} ya existe")
     
--- distancia/base.py
+++ distancia/base.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+# Base para el programa distancia
 # Escrito por: Juan Carlos 1987
 # Correo: k.juno.1987@gmail.com
+# Fecha: 2019-02-07 01:54:28.060140
 # 
